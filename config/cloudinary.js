cc86c64a9fa3ef9e370647a86b3af1abad649284
Dev Bhattacharya <devbhattacharya42@gmail.com>
2025-04-02 13:01:20 +0530

    cloudinary secured with signature

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,5 +1,6 @@
 const cloudinary = require("cloudinary").v2;
 const dotenv = require("dotenv");
+const crypto = require("crypto");
 dotenv.config();
 const cloudinaryConfig = () => {
   cloudinary.config({
@@ -8,11 +9,31 @@ const cloudinaryConfig = () => {
     api_secret: process.env.CLOUDINARY_API_SECRET, // ✅ FIXED
   });
 };
+
+const generateTimeStamp = () => {
+  const timestamp = Math.round(new Date().getTime() / 1000);
+  const api_secret = process.env.CLOUDINARY_API_SECRET;
+
+  // generate the signature string
+  const signatureString = `folder=upload&timestamp=${timestamp}${api_secret}`;
+
+  // generate a SHA1 hash for our signature
+  const signature = crypto
+    .createHash("sha1")
+    .update(signatureString)
+    .digest("hex");
+  return { timestamp, signature };
+};
+
 const cloudinaryUpload = async (filePath) => {
   try {
     cloudinaryConfig();
+    const { timestamp, signature } = generateTimeStamp();
     const result = await cloudinary.uploader.upload(filePath, {
       folder: "upload",
+      timestamp: timestamp,
+      signature: signature,
+      api_key: process.env.CLOUDINARY_API_KEY, // API Key is required
     });
     return result;
   } catch (error) {
